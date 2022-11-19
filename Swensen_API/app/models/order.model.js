6a364b02f0cad36acf4dcd85d0f0f003c6c79d78
Tiaggaws7 <tiagobuil.tb@gmail.com>
2022-11-19 15:07:08 +0700

    CRUD for Orders

--- Swensen_API/app/models/order.model.js
+++ Swensen_API/app/models/order.model.js
@@ -1,9 +1,76 @@
-
+const sql = require("./db");
 
 //Constructor
 const Order = function (order) {
-    this.cartID = order.cartID;
-    this.customerID = order.customerID;
+    this.cartId = order.cartId;
+    this.customerId = order.customerId;
     this.status = order.status;
     this.date = order.date;
 };
+
+Order.getAllRecords = (result) => {
+    sql.query("SELECT * FROM Orders", (err, res) => {
+        if (err) {
+            console.log("Query error: " + err);
+            result(err, null);
+            return;
+        }
+        result(null, res);
+    });
+};
+
+Order.create = ( newOrder, result ) => {
+    sql.query("INSERT INTO Orders SET ?", newOrder, (err, res) => {
+        if (err) {
+            console.log("Query error: " + err);
+            result(err, null);
+            return;
+        }
+        console.log("Created Order: ", {
+            id: res.insertId,
+            ...newOrder
+        });
+        result(null, {
+            id: res.insertId,
+            ...newOrder
+        });
+    })
+}
+
+Order.updateByID = (id, data, result) => {
+    sql.query(
+        "UPDATE Orders SET cartId=?, customerId=?, status=?, date=? WHERE id=?",
+        [data.cartId, data.customerId, data.status, data.date, id],
+        (err, res) => {
+            if (err) {
+                console.log("Query error: " + err);
+                result(err, null);
+                return;
+            }
+            if (res.affectedRows == 0) {
+                //this id not found
+                result({ kind: "not_found" }, null);
+                return;
+            }
+            console.log("Updated Order: ", { id: id, ...data });
+            result(null, { id: id, ...data });
+        }
+    );
+};
+
+Order.delete = ( id, result ) => {
+    sql.query("DELETE FROM Orders WHERE id = ?", id, (err, res) => {
+        if (err) {
+            console.log("Query error: " + err);
+            result(err, null);
+            return;
+        }    if(res.affectedRows == 0){
+            result({kind: "not_found"}, null)
+            return;
+        }
+        console.log("Deleted Order id: ", id)
+        result(null, {id: id})
+    });
+}
+
+module.exports = Order
--- Swensen_API/app/routes/order.routes.js
+++ Swensen_API/app/routes/order.routes.js
@@ -1,8 +1,10 @@
-const authJWT = require("../middleware/auth.jwt")
-const order_controller = require("../controllers/order.controller");
 module.exports = (app) => {
     const order_controller = require("../controllers/order.controller")
     var router = require("express").Router();
-    //router.post("/add", order_controller.createNewCategory);
+    router.post("/add", order_controller.createNewOrder);
+    router.get("/all", order_controller.getAllOrder);
+    router.put("/:id", order_controller.updateOrder);
+    router.delete("/:id", order_controller.deleteOrder);
+
     app.use("/api/order", router);
 };
